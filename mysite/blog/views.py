6fde9b95ae4fba568d0dbbb65737453ea03c9974
Harsh Kumar <harshkumar9.in@gmail.com>
2020-03-23 00:19:26 +0530

    home page created

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+
+
+posts=[
+    {
+        'author': 'Harsh',
+        'title': 'Blog Post',
+        'content' : 'First post content',
+        'date_posted': 'March 22,2020 '
+    },
+    {
+        'author': 'Harsh',
+        'title': 'Blog Post',
+        'content' : 'Second post content',
+        'date_posted': 'March 23,2020 '
+    }
+]
+
+def home(request):
+    context={
+        'post': posts
+    }
+    return render(request,'blog/home.html', context)
+
+def about(request):
+    return render(request,'blog/about.html')
