e92b2b664c4b78d35e86013f78d39f0d99c4a7a1
Harsh Kumar <harshkumar9.in@gmail.com>
2020-03-25 11:20:02 +0530

    database created

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from .models import post
+
 # Register your models here.
+
+admin.site.register(post)
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,28 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import post
 
 
 # Create your views here.
 
 
-posts=[
-    {
-        'author': 'Harsh',
-        'title': 'Blog Post',
-        'content' : 'First post content',
-        'date_posted': 'March 22,2020 '
-    },
-    {
-        'author': 'Harsh',
-        'title': 'Blog Post',
-        'content' : 'Second post content',
-        'date_posted': 'March 23,2020 '
-    }
-]
 
 def home(request):
     context={
-        'post': posts
+        'post': post.objects.all()
     }
     return render(request,'blog/home.html', context)
 
