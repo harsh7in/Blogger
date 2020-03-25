079ffedfe19173389a7bec9b8e0f1a06d2d4bcfc
Harsh Kumar <harshkumar9.in@gmail.com>
2020-03-25 13:08:41 +0530

    basic form added

--- mysite/users/views.py
+++ mysite/users/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render ,redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 # Create your views here.
+
+
+def register(request):
+    if request.method=='POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username= form.cleaned_data.get('username')
+            messages.success(request,f'Account created for {username}!')
+            return redirect('blog-home')
+    else:
+        form=UserCreationForm()
+
+    return render(request,'user/register.html',{'form': form})
+
