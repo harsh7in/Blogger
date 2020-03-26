d9876a225dc122723330735db8702f6373551e5e
Harsh Kumar <harshkumar9.in@gmail.com>
2020-03-26 11:16:09 +0530

    form modified

--- mysite/users/views.py
+++ mysite/users/views.py
@@ -1,20 +1,19 @@
 from django.shortcuts import render ,redirect
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
-
+from .forms import user_reg_form
 # Create your views here.
 
 
 def register(request):
     if request.method=='POST':
-        form = UserCreationForm(request.POST)
+        form = user_reg_form(request.POST)
         if form.is_valid():
             form.save()
             username= form.cleaned_data.get('username')
             messages.success(request,f'Account created for {username}!')
             return redirect('blog-home')
     else:
-        form=UserCreationForm()
+        form=user_reg_form()
 
     return render(request,'user/register.html',{'form': form})
 
