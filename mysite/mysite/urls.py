3e17820038889e0d272e05e25093adf92b314f1a
Harsh Kumar <harshkumar9.in@gmail.com>
2020-03-26 12:43:58 +0530

    Login Page added

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -17,8 +17,13 @@ from django.contrib import admin
 from django.urls import path,include
 from users import views as user_views
 
+from django.contrib.auth import views as auth_views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
     path('register/', user_views.register,name='register'),
+
+    path('login/', auth_views.LoginView.as_view(template_name='user/login.html'),name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='user/logout.html'),name='logout'),
 ]
