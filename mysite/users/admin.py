e6fcf7bc54887c3aa66cb884deb88e07103fada4
Harsh Kumar <harshkumar9.in@gmail.com>
2020-03-29 16:17:38 +0530

    profile image database created

--- mysite/users/admin.py
+++ mysite/users/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from .models import profile
 # Register your models here.
+
+admin.site.register(profile)
--- mysite/users/models.py
+++ mysite/users/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+class profile(models.Model):
+    user= models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='profile_pics',default='default.jpg')
+    
+    def __str__(self):
+        return f'{self.user.username} profile'
