55c7b866d8f391c5a1b1f657ba9125bc03338531
nooras <anoorasfatima@gmail.com>
2020-08-16 12:54:26 +0530

    Profile picture added and updated option done

--- mysite/users/views.py
+++ mysite/users/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render ,redirect
+# from django.shortcuts import render, get_object_or_404, get_list_or_404
+from django.shortcuts import render ,redirect, get_object_or_404, get_list_or_404
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .forms import user_reg_form
-# Create your views here.
+from .forms import user_reg_form, UserProfileForm
+from .models import Profile
+from django.contrib.auth.models import User
 
+# Create your views here.
 
 def register(request):
     if request.method=='POST':
@@ -12,12 +15,45 @@ def register(request):
             form.save()
             username= form.cleaned_data.get('username')
             messages.success(request,f'Your Account has been created! You are now able to Login')
-            return redirect('Login')
+            return redirect('profile')
     else:
         form=user_reg_form()
 
     return render(request,'user/register.html',{'form': form})
 
+#If User Add New Image to profile
 @login_required
 def profile(request):
-    return render(request, 'user/profile.html')
+    if request.method == 'POST':
+        profile_form = UserProfileForm(request.POST or None, request.FILES or None)
+        if profile_form.is_valid():
+            profile = profile_form.save(commit=False)
+            try:
+                profile.user =  request.user #.profile
+            except Profile.DoesNotExist:
+                profile.user = Profile(user=request.user)
+            if profile_form.cleaned_data['image']:
+                profile.picture = profile_form.cleaned_data["image"]
+            else:
+                messages.success(request,f'Something is wrong, try again')
+            profile.save()
+        else:
+            print(profile_form.errors)
+    else:
+        profile_form = UserProfileForm()
+
+    return render(request,'user/profile.html',{'form': profile_form})
+
+#If User Update Profile Image
+@login_required
+def profileUpdate(request, pk):
+    if request.method == 'POST':
+        #print(pk)
+        server = get_object_or_404(Profile, pk=pk)
+        form = UserProfileForm(request.POST or None, request.FILES or None,  instance=server)
+        if form.is_valid():
+            edit = form.save(commit=False)
+            edit.save()
+            return redirect('profile')
+    form = UserProfileForm()
+    return render(request,'user/profile.html',{'form': form})
