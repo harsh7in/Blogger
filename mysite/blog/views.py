b2f255b0d06a5e0a0432233f7d4a9c63c536a76d
nooras <anoorasfatima@gmail.com>
2020-08-16 20:41:55 +0530

    Post create(Title, Content) done

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,17 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import post
-
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 
-
-
 def home(request):
     context={
-        'post': post.objects.all()
+        'post': Post.objects.all()
     }
     return render(request,'blog/home.html', context)
 
 def about(request):
     return render(request,'blog/about.html')
+
+@login_required
+def post_create(request):
+    form = PostForm(request.POST or None, request.FILES or None)
+    if form.is_valid():
+        instance = form.save(commit=False)
+        instance.author_id = request.user.id
+        instance.save()
+        messages.success(request, "Successfully Created")
+        return redirect('blog-home')
+    context  ={
+        "form": form
+    }
+    return render(request, "blog/post_create.html", context)
