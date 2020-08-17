8aa1f608c7b9e435a35a218f9ad660a8ee6710ce
Akshita Agarwal <akshita.ag321@gmail.com>
2020-08-18 03:13:44 +0530

    Added profileview template that show profile on clicking author name.

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home ,name="blog-home"),
-    path('about/',views.about,name='blog-about')
+    path('about/',views.about,name='blog-about'),
+    path('profileview/<name>',views.Profileview,name='blog-profile')
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import post
-
-
+from django.contrib.auth.models import User
 # Create your views here.
 
 
@@ -15,3 +14,17 @@ def home(request):
 
 def about(request):
     return render(request,'blog/about.html')
+
+
+def Profileview(request,name):
+    user =User.objects.get(username=name)
+    context={
+        'user':user,
+    }
+    if request.user!=user:
+        return render(request,'blog/profileview.html', context)
+    else:
+        context={
+            'post': post.objects.all()
+        }
+        return render(request,'blog/home.html',context)
