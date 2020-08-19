1774db0d12ec2b1e5c99222ea05fa08c6fd6e0e0
Joshitha18 <joshitha1830@gmail.com>
2020-08-19 09:42:29 +0530

    Resolved all conflicts

--- mysite/myvenv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/sysad/Downloads/Projects/HakinCodes/Blogger/mysite/myvenv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
