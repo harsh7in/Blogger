0725c149e1d44d63ff801f3291315aa5c7bb5369
shraddha <anitha.shraddha@gmail.com>
2020-08-23 10:35:08 +0530

    updated

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,14 +1,20 @@
 from django.urls import path
-from . import views
-from .views import PostDetailView, PostUpdateView,PostDeleteView
+from .views import (
+        PostDetailView,
+        PostUpdateView,
+        PostDeleteView,
+        home,
+        about,
+        post_create,
+        Profileview
+    )
 
 urlpatterns = [
-    path('', views.home ,name="blog-home"),
-    path('about/',views.about,name='blog-about'),
-    path('profileview/<name>',views.Profileview,name='blog-profile'),
-    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
-    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
-    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
-    path("post_create/", views.post_create, name='post_create'),
+    path("", home, name="blog-home"),
+    path("about/", about, name="blog-about"),
+    path("profileview/<name>", Profileview, name="blog-profile"),
+    path("post/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
+    path("post/<int:pk>/update/", PostUpdateView.as_view(), name="post-update"),
+    path("post/<int:pk>/delete/", PostDeleteView.as_view(), name="post-delete"),
+    path("post_create/", post_create, name="post_create"),
 ]
-
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -136,3 +136,11 @@ LOGIN_URL='login'
 
 
 django_heroku.settings(locals())
+
+# SMTP configuration
+EMAIL_USE_TLS = True
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST ='smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER ='from@gmail.com'
+EMAIL_HOST_PASSWORD ='password'
