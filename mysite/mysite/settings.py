b65219349ec67e700951a776d57350099b11558f
kritikaparmar-programmer <kprogrammer07@gmail.com>
2020-09-23 02:14:30 +0530

    Resolved Image issue #167

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -150,13 +150,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR,'static')
+
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
-MEDIA_ROOT=os.path.join(BASE_DIR,'media')
-
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL='/media/'
 
 LOGIN_REDIRECT_URL='blog-home'
