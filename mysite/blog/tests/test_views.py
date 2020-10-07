ba1e7c36e5468b79a7226712be2760e20684fee5
ManishShah120 <mkshah141@gmail.com>
2020-10-08 00:54:48 +0530

    Done

--- mysite/blog/tests/test_views.py
+++ mysite/blog/tests/test_views.py
@@ -8,7 +8,7 @@ from django.contrib.auth.models import User
 class TestViews(TestCase):
 
     def setUp(self):
-        self.u=User.objects.create_user(username="testuser",email="a@c.in",password="secret") # Create an User instance for performing the tests
+        self.u=User.objects.create_superuser(username="testuser",email="a@c.in",password="secret") # Create an User instance for performing the tests
         self.client = Client()
 
         # URL's of different view
@@ -16,7 +16,9 @@ class TestViews(TestCase):
         self.viewcount_url = reverse('getBlogs')
         self.detailview_url = reverse('post-detail', args=['testblog1'])
         self.homeview_url = reverse('blog-home')
-        self.deleteview_url = reverse('post-delete', args=[1])
+        self.profileview_url = reverse('blog-profile', args=[self.u.username])
+        self.updateview_url = reverse('post-update', args=[1])
+        self.postcreate_url = reverse('post_create')
 
         # Creating instances of the Model
         self.testblog1 = Post.objects.create(
@@ -67,41 +69,21 @@ class TestViews(TestCase):
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'blog/home.html')
 
-    # Code for testing the `PostDelete` View [class based view]
-    # def test_postdelete_view_GET(self):
-    #     deletepost1 = Post.objects.create(
-    #         title       = 'testblogfordelte',
-    #         content     = 'This is only for testing purpose blog',
-    #         author      = User.objects.get(email='a@c.in'),
-    #         view_count  = 0,
-    #     )
-    #     deletepost1.delete()
-    #     response = self.client.delete(self.deleteview_url)
-    #     self.assertEquals(response.status_code, 204)
+    # Code for testing the `Profileview` View
+    def test_profileview_GET(self):
+        response = self.client.get(self.profileview_url)
+        self.assertEquals(self.u.username, 'testuser')
+        self.assertTemplateUsed(response, 'user/profile.html')
 
+    # Code for testing the `postUpdatView` View
+    def test_postupdateview_GET(self):
+        self.testblog1.title = 'updating the title'
+        self.assertNotEquals(self.testblog1.title, 'testblog1')
 
-
-
-
-
-
-
-
-
-# Name of the View functions
-
-
-
-# 4. Profileview []
-# 6. PostUpdateView []
-
-# 8. post_create []
-
-
-
-
-# 7. PostDeleteView []
-
-
-
-# After all the tests scripts are written order them according to the order of their view function in view.py file
+    # Code for testing the `post_create` View
+    # def testpostcreateview_GET(self):
+    #     self.client.login(username='testuser', password='secret')
+    #     # self.tusu = Post.objects.create(title= 'this created by the client',content='this is the content of the blog-post')    
+    #     print(Post.objects.all().count())
+    #     # self.assertEquals()
+    #     # self.assertEquals(response, True)
